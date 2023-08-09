c05ddabc1bc9d20e0b788f19af09adeae6bff317
jgcodes2020 <jackyguo18@hotmail.com>
2023-08-08 21:39:13 -0400

    Delete unused stuff

--- TinCan.NET/Program.cs
+++ TinCan.NET/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Avalonia;
 using System;
+using System.ComponentModel;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Threading;
 using TinCan.NET.Models;
 
@@ -14,6 +16,7 @@ class Program
     [STAThread]
     public static void Main(string[] args)
     {
+        SetupConsole();
         BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
     }
 
@@ -22,4 +25,24 @@ class Program
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .LogToTrace();
+    
+    
+    [DllImport("kernel32.dll", EntryPoint = "AttachConsole", SetLastError = true)]
+    [return : MarshalAs(UnmanagedType.Bool)]
+    private static extern bool Win32_AttachConsole(uint dwProcessId);
+
+    private const uint Win32_AttachParentProcess = uint.MaxValue;
+
+    private static void SetupConsole()
+    {
+        if (!OperatingSystem.IsWindows())
+            return;
+
+        if (Win32_AttachConsole(Win32_AttachParentProcess))
+            return;
+        
+        int err = Marshal.GetLastPInvokeError();
+        if (err != 6)
+            throw new Win32Exception(err);
+    }
 }
