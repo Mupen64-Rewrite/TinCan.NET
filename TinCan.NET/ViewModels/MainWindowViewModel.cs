a79b95e5f1b1770c443c95ebd7a5782dfcf30c43
Alex <48759429+Aurumaker72@users.noreply.github.com>
2023-10-15 22:51:56 +0200

    Improve rapidfire behaviour and visual representation

--- TinCan.NET/ViewModels/MainWindowViewModel.cs
+++ TinCan.NET/ViewModels/MainWindowViewModel.cs
@@ -45,6 +45,9 @@ public partial class MainWindowViewModel : ObservableObject
 
     private void SetButtonMask(Buttons.Mask bit, bool value, [CallerMemberName] string? callerMemberName = null)
     {
+        // if button state is manually changed, we remove this button's rapidfire flag
+        _rapidFireMask &= ~bit;
+
         OnPropertyChanging(callerMemberName);
         _buttonMask = (_buttonMask & ~bit) | (value ? bit : 0);
         OnPropertyChanged(callerMemberName);
@@ -52,85 +55,85 @@ public partial class MainWindowViewModel : ObservableObject
 
     public bool BtnDUp
     {
-        get => (_buttonMask & Buttons.Mask.DUp) != 0;
+        get => (Value & (int)Buttons.Mask.DUp) != 0;
         set => SetButtonMask(Buttons.Mask.DUp, value);
     }
 
     public bool BtnDDown
     {
-        get => (_buttonMask & Buttons.Mask.DDown) != 0;
+        get => (Value & (int)Buttons.Mask.DDown) != 0;
         set => SetButtonMask(Buttons.Mask.DDown, value);
     }
 
     public bool BtnDLeft
     {
-        get => (_buttonMask & Buttons.Mask.DLeft) != 0;
+        get => (Value & (int)Buttons.Mask.DLeft) != 0;
         set => SetButtonMask(Buttons.Mask.DLeft, value);
     }
 
     public bool BtnDRight
     {
-        get => (_buttonMask & Buttons.Mask.DRight) != 0;
+        get => (Value & (int)Buttons.Mask.DRight) != 0;
         set => SetButtonMask(Buttons.Mask.DRight, value);
     }
 
     public bool BtnCUp
     {
-        get => (_buttonMask & Buttons.Mask.CUp) != 0;
+        get => (Value & (int)Buttons.Mask.CUp) != 0;
         set => SetButtonMask(Buttons.Mask.CUp, value);
     }
 
     public bool BtnCDown
     {
-        get => (_buttonMask & Buttons.Mask.CDown) != 0;
+        get => (Value & (int)Buttons.Mask.CDown) != 0;
         set => SetButtonMask(Buttons.Mask.CDown, value);
     }
 
     public bool BtnCLeft
     {
-        get => (_buttonMask & Buttons.Mask.CLeft) != 0;
+        get => (Value & (int)Buttons.Mask.CLeft) != 0;
         set => SetButtonMask(Buttons.Mask.CLeft, value);
     }
 
     public bool BtnCRight
     {
-        get => (_buttonMask & Buttons.Mask.CRight) != 0;
+        get => (Value & (int)Buttons.Mask.CRight) != 0;
         set => SetButtonMask(Buttons.Mask.CRight, value);
     }
 
     public bool BtnA
     {
-        get => (_buttonMask & Buttons.Mask.A) != 0;
+        get => (Value & (int)Buttons.Mask.A) != 0;
         set => SetButtonMask(Buttons.Mask.A, value);
     }
 
     public bool BtnB
     {
-        get => (_buttonMask & Buttons.Mask.B) != 0;
+        get => (Value & (int)Buttons.Mask.B) != 0;
         set => SetButtonMask(Buttons.Mask.B, value);
     }
 
     public bool BtnStart
     {
-        get => (_buttonMask & Buttons.Mask.Start) != 0;
+        get => (Value & (int)Buttons.Mask.Start) != 0;
         set => SetButtonMask(Buttons.Mask.Start, value);
     }
 
     public bool BtnZ
     {
-        get => (_buttonMask & Buttons.Mask.Z) != 0;
+        get => (Value & (int)Buttons.Mask.Z) != 0;
         set => SetButtonMask(Buttons.Mask.Z, value);
     }
 
     public bool BtnL
     {
-        get => (_buttonMask & Buttons.Mask.L) != 0;
+        get => (Value & (int)Buttons.Mask.L) != 0;
         set => SetButtonMask(Buttons.Mask.L, value);
     }
 
     public bool BtnR
     {
-        get => (_buttonMask & Buttons.Mask.R) != 0;
+        get => (Value & (int)Buttons.Mask.R) != 0;
         set => SetButtonMask(Buttons.Mask.R, value);
     }
 }
